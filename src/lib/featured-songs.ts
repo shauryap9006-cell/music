23d92ebb9c793d83ce18832e7b0e4b5ae461debd
shauryap9006-cell <shauryap9006@users.noreply.github.com>
2026-04-14 00:18:57 +0530

    fix(hero): update featured songs to point to existing files

--- src/lib/featured-songs.ts
+++ src/lib/featured-songs.ts
@@ -7,123 +7,123 @@ export interface FeaturedSong {
 
 export const FEATURED_SONGS: FeaturedSong[] = [
   { 
-    id: "local-1", 
-    title: "100 Million", 
-    artist: "Karan Aujla", 
-    poster_url: "/api/songs/artwork?scope=library&name=100%20Million_spotdown.org.mp3" 
-  },
-  { 
-    id: "local-2", 
+    id: "f-1", 
     title: "2 AM", 
     artist: "Karan Aujla", 
     poster_url: "/api/songs/artwork?scope=library&name=2%20AM_spotdown.org.mp3" 
   },
   { 
-    id: "local-3", 
-    title: "Born to Shine", 
-    artist: "Diljit Dosanjh", 
-    poster_url: "/api/songs/artwork?scope=library&name=Born%20to%20Shine_spotdown.org.mp3" 
+    id: "f-2", 
+    title: "52 Bars", 
+    artist: "Karan Aujla", 
+    poster_url: "/api/songs/artwork?scope=preview&name=52%20Bars.mp3" 
   },
   { 
-    id: "local-4", 
-    title: "Brown Munde", 
-    artist: "AP Dhillon", 
-    poster_url: "/api/songs/artwork?scope=library&name=Brown%20Munde_spotdown.org.mp3" 
+    id: "f-3", 
+    title: "Admirin' You", 
+    artist: "Karan Aujla", 
+    poster_url: "/api/songs/artwork?scope=preview&name=Admirin'%20You.mp3" 
   },
   { 
-    id: "local-5", 
+    id: "f-4", 
+    title: "Cheques", 
+    artist: "Shubh", 
+    poster_url: "/api/songs/artwork?scope=preview&name=Cheques.mp3" 
+  },
+  { 
+    id: "f-5", 
     title: "Case", 
     artist: "Diljit Dosanjh", 
     poster_url: "/api/songs/artwork?scope=library&name=Case_spotdown.org.mp3" 
   },
   { 
-    id: "local-6", 
-    title: "Champion's Anthem", 
-    artist: "Karan Aujla", 
-    poster_url: "/api/songs/artwork?scope=library&name=Champion's%20Anthem_spotdown.org.mp3" 
-  },
-  { 
-    id: "local-7", 
-    title: "Cheques", 
-    artist: "Shubh", 
-    poster_url: "/api/songs/artwork?scope=library&name=Cheques_spotdown.org.mp3" 
-  },
-  { 
-    id: "local-8", 
-    title: "Elevated", 
-    artist: "Shubh", 
-    poster_url: "/api/songs/artwork?scope=library&name=Elevated_spotdown.org.mp3" 
-  },
-  { 
-    id: "local-9", 
+    id: "f-6", 
     title: "Excuses", 
     artist: "AP Dhillon", 
-    poster_url: "/api/songs/artwork?scope=library&name=Excuses_spotdown.org.mp3" 
+    poster_url: "/api/songs/artwork?scope=preview&name=Excuses.mp3" 
   },
   { 
-    id: "local-10", 
-    title: "G.O.A.T.", 
-    artist: "Diljit Dosanjh", 
-    poster_url: "/api/songs/artwork?scope=library&name=G.O.A.T._spotdown.org.mp3" 
-  },
-  { 
-    id: "local-11", 
-    title: "Hint", 
+    id: "f-7", 
+    title: "On Top", 
     artist: "Karan Aujla", 
-    poster_url: "/api/songs/artwork?scope=library&name=Hint_spotdown.org.mp3" 
+    poster_url: "/api/songs/artwork?scope=preview&name=On%20Top.mp3" 
   },
   { 
-    id: "local-12", 
-    title: "Insane", 
-    artist: "AP Dhillon", 
-    poster_url: "/api/songs/artwork?scope=library&name=Insane_spotdown.org.mp3" 
-  },
-  { 
-    id: "local-13", 
-    title: "Lover", 
-    artist: "Diljit Dosanjh", 
-    poster_url: "/api/songs/artwork?scope=library&name=Lover_spotdown.org.mp3" 
-  },
-  { 
-    id: "local-14", 
-    title: "No Love", 
-    artist: "Shubh", 
-    poster_url: "/api/songs/artwork?scope=library&name=No%20Love_spotdown.org.mp3" 
-  },
-  { 
-    id: "local-15", 
-    title: "Old Skool", 
-    artist: "Prem Dhillon", 
-    poster_url: "/api/songs/artwork?scope=library&name=Old%20Skool_spotdown.org.mp3" 
-  },
-  { 
-    id: "local-16", 
-    title: "Players", 
-    artist: "Badshah & Karan Aujla", 
-    poster_url: "/api/songs/artwork?scope=library&name=Players_spotdown.org.mp3" 
-  },
-  { 
-    id: "local-17", 
-    title: "So High", 
-    artist: "Sidhu Moose Wala", 
-    poster_url: "/api/songs/artwork?scope=library&name=So%20High_spotdown.org.mp3" 
-  },
-  { 
-    id: "local-18", 
+    id: "f-8", 
     title: "Softly", 
     artist: "Karan Aujla", 
-    poster_url: "/api/songs/artwork?scope=library&name=Softly_spotdown.org.mp3" 
+    poster_url: "/api/songs/artwork?scope=preview&name=Softly.mp3" 
   },
   { 
-    id: "local-19", 
-    title: "Vibe", 
-    artist: "Diljit Dosanjh", 
-    poster_url: "/api/songs/artwork?scope=library&name=Vibe_spotdown.org.mp3" 
+    id: "f-9", 
+    title: "Players", 
+    artist: "Badshah & Karan Aujla", 
+    poster_url: "/api/songs/artwork?scope=preview&name=Players.mp3" 
   },
   { 
-    id: "local-20", 
+    id: "f-10", 
+    title: "Try Me", 
+    artist: "Karan Aujla", 
+    poster_url: "/api/songs/artwork?scope=preview&name=Try%20Me.mp3" 
+  },
+  { 
+    id: "f-11", 
     title: "Winning Speech", 
     artist: "Karan Aujla", 
-    poster_url: "/api/songs/artwork?scope=library&name=Winning%20Speech_spotdown.org.mp3" 
+    poster_url: "/api/songs/artwork?scope=preview&name=Winning%20Speech.mp3" 
+  },
+  { 
+    id: "f-12", 
+    title: "Take It Easy", 
+    artist: "Karan Aujla", 
+    poster_url: "/api/songs/artwork?scope=preview&name=Take%20It%20Easy.mp3" 
+  },
+  { 
+    id: "f-13", 
+    title: "Arrogant", 
+    artist: "Karan Aujla", 
+    poster_url: "/api/songs/artwork?scope=library&name=Arrogant_spotdown.org.mp3" 
+  },
+  { 
+    id: "f-14", 
+    title: "Guilty", 
+    artist: "Karan Aujla", 
+    poster_url: "/api/songs/artwork?scope=library&name=Guilty_spotdown.org.mp3" 
+  },
+  { 
+    id: "f-15", 
+    title: "Old Money", 
+    artist: "Sidhu Moose Wala", 
+    poster_url: "/api/songs/artwork?scope=preview&name=Old%20Money.mp3" 
+  },
+  { 
+    id: "f-16", 
+    title: "Police", 
+    artist: "Karan Aujla", 
+    poster_url: "/api/songs/artwork?scope=preview&name=Police.mp3" 
+  },
+  { 
+    id: "f-17", 
+    title: "Tell Me", 
+    artist: "Karan Aujla", 
+    poster_url: "/api/songs/artwork?scope=library&name=Tell%20Me_spotdown.org.mp3" 
+  },
+  { 
+    id: "f-18", 
+    title: "Nothing Lasts", 
+    artist: "Karan Aujla", 
+    poster_url: "/api/songs/artwork?scope=library&name=Nothing%20Lasts_spotdown.org.mp3" 
+  },
+  { 
+    id: "f-19", 
+    title: "Life is Short", 
+    artist: "Karan Aujla", 
+    poster_url: "/api/songs/artwork?scope=library&name=Life%20is%20Short_spotdown.org.mp3" 
+  },
+  { 
+    id: "f-20", 
+    title: "Na Na Na", 
+    artist: "Karan Aujla", 
+    poster_url: "/api/songs/artwork?scope=library&name=Na%20Na%20Na_spotdown.org.mp3" 
   },
 ];
